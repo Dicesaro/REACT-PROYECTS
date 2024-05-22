290e1df13a5d8d4d59ac800b578336e2a6ca3668
Cesar <cesarluis4056@gmail.com>
2024-05-22 18:18:56 -0500

    feat: :memo: first commit

---
+++ gsap-react/src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import Gsap from "./Componentes/Gsap/gsap.jsx";
+import Estado from "./Componentes/Estado/estado.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* <Gsap /> */}
+    <Estado />
+  </React.StrictMode>
+);
