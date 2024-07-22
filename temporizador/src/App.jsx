c3f00924ec482ca4ebacd2cd4ebc35b58474dd69
Cesar Ortega <cesarluis4056@gmail.com>
2024-07-21 19:43:37 -0500

    GRILLA COMPLETADA

--- temporizador/src/App.jsx
+++ temporizador/src/App.jsx
@@ -8,9 +8,9 @@ export function App() {
       <h1>Paises del mundo</h1>
       <section className="cont_countrys">
         {country?.map((country) => (
-          <div className="info_countrys" key={country.name.common}>
-            <h2>{country.name.common}</h2>
-            <img src={country.flags.png} alt={country.name.common} />
+          <div className="info_countrys" key={country.name}>
+            <img src={country.flags} alt={country.name} />
+            <h2>{country.name}</h2>
           </div>
         ))}
       </section>
--- temporizador/src/hooks/useFlags.js
+++ temporizador/src/hooks/useFlags.js
@@ -5,11 +5,16 @@ const API_FLAGS = "https://restcountries.com/v3.1/all?fields=name,flags";
 export function useFlags() {
   const [country, setCountry] = useState([]);
 
+  const mappedFlags = country?.map((country) => ({
+    name: country.name.common,
+    flags: country.flags.png,
+  }));
+
   useEffect(() => {
     fetch(API_FLAGS)
       .then((res) => res.json())
       .then((data) => setCountry(data));
   }, []);
 
-  return country;
+  return { country: mappedFlags };
 }
